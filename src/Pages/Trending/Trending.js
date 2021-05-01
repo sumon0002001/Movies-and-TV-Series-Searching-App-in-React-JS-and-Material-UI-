37591f999351f19fb64ddf5f9f147e009516bb45
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-05-02 03:15:18 +0600

    Fetch the data from Api

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,4 +1,19 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 const Trending = () => {
+
+
+    const fetchTrending = async () => {
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/trending/all/day?api_key=7530ed1f2cdb5da9d81039f6b9fcb16d`
+      )
+      console.log(data);
+    }
+
+    useEffect(() => {
+        fetchTrending();
+    }, [])
   return (
     <div>
       <span className="title">This Trending Page</span>

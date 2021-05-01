ad01361d82f8aa5a16b338abc82f6ce93c39fe8b
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-05-02 04:43:24 +0600

    Bring the movie details

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,14 +1,16 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import SingleContent from "../../components/SingleContent/SingleContent";
 
 const Trending = () => {
-
-
+  const [content, setContent] = useState([]);
     const fetchTrending = async () => {
       const { data } = await axios.get(
         `https://api.themoviedb.org/3/trending/all/day?api_key=7530ed1f2cdb5da9d81039f6b9fcb16d`
       )
-      console.log(data);
+     
+
+      setContent(data.results)
     }
 
     useEffect(() => {
@@ -17,6 +19,19 @@ const Trending = () => {
   return (
     <div>
       <span className="title">This Trending Page</span>
+      <div>
+          {content && content.map((c) => 
+              < SingleContent 
+                key={c.id}
+                id={c.id}
+                poster={c.poster_path}
+                title={c.title || c.name}
+                date={c.first_air_date || c.release_date} 
+                media_type={c.media_type}
+                vote_average={c.vote_average}
+              />
+          )}
+      </div>
     </div>
   )
 }

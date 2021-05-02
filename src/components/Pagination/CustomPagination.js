21339cc478ffc1cd71354f92a8c43d284076a51c
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-05-03 01:48:42 +0600

    Add Theme to the trending page

--- src/components/Pagination/CustomPagination.js
+++ src/components/Pagination/CustomPagination.js
@@ -1,4 +1,13 @@
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import { Palette } from '@material-ui/icons';
 import Pagination from '@material-ui/lab/Pagination';
+
+
+const darkTheme = createMuiTheme ({
+  Palette: {
+    type: 'dark'
+  },
+})
 
 
 const CustomPagination = ({setPage, numberOfPages = 10}) => {
@@ -8,11 +17,25 @@ const CustomPagination = ({setPage, numberOfPages = 10}) => {
     window.scroll(0, 0);
   }
   return (
-      <div>
-         <Pagination 
+      <div
+         style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        marginTop: 10,
+      }} 
+      >
+        <ThemeProvider theme={darkTheme} >
+        <Pagination 
          count = {numberOfPages}
          onChange = {(e) => handlePagechange(e.target.textContent)} 
+         color="primary"
+         hideNextButton
+         hidePrevButton
          />
+
+        </ThemeProvider>
+         
       </div>
   )
 }
